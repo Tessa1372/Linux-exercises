8360a3412a0b299530755b8f4240f3a03e22bbc2
Tessa S Mampilly <148181088+Tessa1372@users.noreply.github.com>
2025-02-20 14:44:40 +0530

    Update age-extracting.sh

--- age-extracting.sh
+++ age-extracting.sh
@@ -2,7 +2,7 @@
 
 mkdir -p ./testing
 cat input.txt
-read delimiter column < input.txt
+read column < input.txt
 tail -n +2 input.txt > ./testing/test.csv
 cut -d "," -f"$column" ./testing/test.csv | tail -n +1 | sort > ./testing/test.txt
 cat ./testing/test.txt
